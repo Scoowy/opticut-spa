21bc329949d34769c304c593dd1e4a81bfc8bdaf
Scoowy <gahonajuanjo@gmail.com>
2021-11-28 12:51:26 -0500

    Prepared a initial skeleton

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-expand navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">OptiCut</a>
+      <div class="navbar-nav">
+        <router-link class="nav-link" to="/">Home</router-link>
+        <router-link class="nav-link" to="/about">About</router-link>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: [],
+};
+</script>
+
+<style></style>
